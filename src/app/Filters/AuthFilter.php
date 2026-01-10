93b1342e275a27e9109024a91b134d57c63ebfbc
moxyom <infdevfil@protonmail.com>
2026-01-10 21:07:52 +0100

    fixing auth filiter

--- src/app/Filters/AuthFilter.php
+++ src/app/Filters/AuthFilter.php
@@ -5,7 +5,8 @@ namespace App\Filters;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
-use App\Enums\UserRole;
+
+helper('auth_helper');
 
 class AuthFilter implements FilterInterface
 {
