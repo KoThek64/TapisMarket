f35c437c022551f7ed460e2cdd9886db7621f11d
Adam BERNARD <Vous@exemple.com>
2026-01-15 11:16:04 +0100

    modif cart_helper

--- src/app/Helpers/cart_helper.php
+++ src/app/Helpers/cart_helper.php
@@ -3,32 +3,32 @@
 use App\Models\CartModel;
 use App\Models\CartItemModel;
 
-if (!function_exists('count_cart_items')) {
-    function count_cart_items(): int
-    {
-        helper('auth');
-        
-        $userId = user_id();
 
-        if ($userId) {
-            $cartModel = new CartModel();
-            
-            $cart = $cartModel->getActiveCart($userId);
-            
-            if (!$cart) return 0;
-            
-            $cartItemModel = new CartItemModel();
-            return $cartItemModel->getTotalItemsCount($cart->id);
-            
-        } else {
-            $session = session();
-            $guestCart = $session->get('guest_cart');
-            
-            if (empty($guestCart) || !is_array($guestCart)) {
-                return 0;
-            }
-            
-            return array_sum($guestCart);
+function count_cart_items(): int
+{
+    helper('auth');
+
+    $userId = user_id();
+
+    if ($userId) {
+        $cartModel = new CartModel();
+
+        $cart = $cartModel->getActiveCart($userId);
+
+        if (!$cart)
+            return 0;
+
+        $cartItemModel = new CartItemModel();
+        return $cartItemModel->getTotalItemsCount($cart->id);
+
+    } else {
+        $session = session();
+        $guestCart = $session->get('guest_cart');
+
+        if (empty($guestCart) || !is_array($guestCart)) {
+            return 0;
         }
+
+        return array_sum($guestCart);
     }
 }
